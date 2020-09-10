770f79ac6f79450b9c5a168d8d1b8d13185d398a
namusanga <namusanganicho@gmail.com>
2020-09-10 10:48:30 +0300

    Initial commit

---
+++ AddNumberEssential.s
@@ -0,0 +1,19 @@
+Add:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	movq	%rsp, %rbp
+	movl	%edi, -20(%rbp)
+	movl	%esi, -24(%rbp)
+	movl	-20(%rbp), %edx
+	movl	-24(%rbp), %eax
+	addl	%edx, %eax
+	movl	%eax, -4(%rbp)
+	movl	-4(%rbp), %eax
+	popq	%rbp
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	Add, .-Add
+	.section	.rodata
---
+++ AddNumbers.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int Add(int a, int b){
+    int sum = a+b;
+    return sum;
+}
+
+int main() {
+    printf("%i \n",Add(10,10));    
+    return 0;
+}
